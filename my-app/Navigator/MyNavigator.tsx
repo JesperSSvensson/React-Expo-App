87d105a6719f1a67aac997c1c9930c1876f9724d
Jesper Svensson <jesper-svensson@hotmail.com>
2023-09-25 12:09:50 +0200

    Added login with password

--- my-app/Navigator/MyNavigator.tsx
+++ my-app/Navigator/MyNavigator.tsx
@@ -1,16 +1,26 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeScreen from "../Screens/Home";
-
+import Login from "../Screens/Login";
 
 export type RootStackParamList = {
-    Home: undefined;
-}
+  Home: undefined;
+  Login: undefined;
+};
 
 const Drawer = createDrawerNavigator<RootStackParamList>();
 
 const MyNavigator = () => {
   return (
     <Drawer.Navigator>
+      <Drawer.Screen
+        options={{
+          drawerItemStyle: { display: "none" },
+          swipeEnabled: false,
+          headerShown: false,
+        }}
+        name="Login"
+        component={Login}
+      />
       <Drawer.Screen name="Home" component={HomeScreen} />
     </Drawer.Navigator>
   );
---
+++ my-app/Screens/Login.tsx
@@ -0,0 +1,88 @@
+import {
+  Text,
+  View,
+  Button,
+  StyleSheet,
+  ScrollView,
+  Image,
+  
+} from "react-native";
+import * as LocalAuthentication from "expo-local-authentication";
+import { BlurView } from "expo-blur";
+
+interface Props {
+  navigation: any;
+}
+
+export default function Login({ navigation }: Props) {
+  const getAccesWithPinCode = async () => {
+    const result = await LocalAuthentication.authenticateAsync({
+      promptMessage: "Logga in med din personliga kod",
+    });
+
+    if (result.success) {
+      alert("Authentication successful!");
+      navigation.navigate("Home");
+    } else {
+      alert("Authentication failed or canceled.");
+    }
+  };
+
+  const uri = "https://images.pexels.com/photos/7125282/pexels-photo-7125282.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri }} style={styles.backgroundImage} />
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <View style={styles.blurContainer}>
+          <BlurView intensity={20} style={styles.blurView} tint="default">
+            <Text style={styles.textContainer}>Välkommen</Text>
+            <Button
+              color="white"
+              title="Logga in"
+              onPress={getAccesWithPinCode}
+            />
+          </BlurView>
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  backgroundImage: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+  },
+  blurContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "white",
+    overflow: "hidden",
+  },
+  scrollViewContent: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  blurView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+});
